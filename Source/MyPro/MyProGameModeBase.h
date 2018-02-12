99b5a610758eacb87d598950d5a292159ec16d5e
Patrick Gourdet <patrickgourdet79@gmail.com>
2018-02-11 19:14:31 -0500

    The base project file

---
+++ Source/MyPro/MyProGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyProGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPRO_API AMyProGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
